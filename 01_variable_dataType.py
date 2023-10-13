c0bdaf07e6b97b752e5ec70ccd271494d3bfd6e9
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-13 10:01:10 -0800

    variable

---
+++ 01_variable_dataType.py
@@ -0,0 +1,13 @@
+shakil = "Md Soliman "  # string data type 
+age = 20  # int data type 
+examPoint = 4.5 # float data type 
+print(type(shakil)) 
+print(type(age))
+print(type(examPoint))
+
+# many value in maltipul variable 
+name , age , state, point  = "alif", 20 , "Dhaka", 3.90
+print(name)
+print(age)
+print(state)
+print(point)
