758b1b4b354d892233bb3bef3a756692cd0716d5
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-13 10:24:32 -0800

    data type

--- 01_variable_dataType.py
+++ 01_variable_dataType.py
@@ -1,13 +1,30 @@
-shakil = "Md Soliman "  # string data type 
-age = 20  # int data type 
-examPoint = 4.5 # float data type 
-print(type(shakil)) 
-print(type(age))
-print(type(examPoint))
+# shakil = "Md Soliman "  # string data type 
+# age = 20  # int data type 
+# examPoint = 4.5 # float data type 
+# print(type(shakil)) 
+# print(type(age))
+# print(type(examPoint))
 
 # many value in maltipul variable 
-name , age , state, point  = "alif", 20 , "Dhaka", 3.90
-print(name)
-print(age)
-print(state)
-print(point)
+# 
+# name , age , state, point  = "alif", 20 , "Dhaka", 3.90
+# print(name)
+# print(age)
+# print(state)
+# print(point) 
+# 
+
+
+# data type in python 
+#  string , int, number , bool, list , dictonary , tuple , set 
+
+
+# number data type 
+# integer data type and float data type 
+# int data = 20 (is a integer data )
+# floating data = 40.5 (is a floate data )
+intData = 20 
+print(intData)
+floatData = 30.5 
+print(floatData)
+# 
