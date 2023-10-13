64c0da5f08cab5b997c4e21133b0079d45c1cc5c
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-13 10:30:12 -0800

    touple and list data type

--- 01_variable_dataType.py
+++ 01_variable_dataType.py
@@ -23,8 +23,16 @@
 # integer data type and float data type 
 # int data = 20 (is a integer data )
 # floating data = 40.5 (is a floate data )
-intData = 20 
-print(intData)
-floatData = 30.5 
-print(floatData)
-# 
+# intData = 20 
+# print(intData)
+# floatData = 30.5 
+# print(floatData)
+shakil = list(("Shakil", "Alif", "Nayeem", "Shohel")) 
+age = list (("20","30", "10", "50")) #list data type 
+shakil2 = tuple(("Alif" ,"20", "Dhaka")) #tuple data type 
+# gratingPoint =  
+# its list type data type 
+
+print(type(shakil))
+print(age)
+print(type(shakil2))
